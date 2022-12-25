7cb2aea328ae744d0c39460620ee65a192029c9a
thanawat.petchsri <thanawat.petchsri@gmail.com>
2022-12-25 19:30:34 +0700

    Fix build

--- dist/button.d.ts
+++
@@ -1,7 +0,0 @@
-import { LitElement } from 'lit';
-export declare class DxButton extends LitElement {
-    type?: string;
-    static styles: import("lit").CSSResult;
-    render(): import("lit-html").TemplateResult<1>;
-}
-//# sourceMappingURL=button.d.ts.map
